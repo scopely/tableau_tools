0541ecde2dc976f343ffa44ea191bf06e720bc8e
Guillem Mercadal Diaz <guillem@scopely.com>
2020-08-11 17:32:19 +0200

    port: Rename tableau tools

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='tableau_tools',
+    name='scopely_tableau_tools',
     python_requires='>=3.6',
     version='5.2.0',
     packages=['tableau_tools', 'tableau_tools.tableau_rest_api', 'tableau_tools.tableau_documents',
               'tableau_tools.examples', 'tableau_tools.tableau_rest_api.methods'],
-    url='https://github.com/bryantbhowell/tableau_tools',
+    url='https://github.com/scopely/tableau_tools',
     license='',
     author='Bryant Howell',
     author_email='bhowell@tableau.com',
     description='A library for programmatically working with Tableau files and Tableau Server, including the REST API',
     install_requires=['requests']
 )
+
